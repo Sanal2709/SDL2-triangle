88fbbb23e10b346ed8c4fbcc42da1e5c3a287409
Azydov Sanal <sanal88@yandex.ru>
2021-04-17 17:52:27 +0300

    Added shell sript

--- .vscode/compilex86.sh
+++ .vscode/compilex86.sh
@@ -1,3 +1,21 @@
-cd build
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ..
+type_dir="$1"
+
+if [ -d "build" ]
+then
+    cd build
+else
+    mkdir build
+    cd build
+fi
+
+if [ -d "$type_dir" ]
+then
+    cd $type_dir
+else
+    mkdir "$type_dir"
+    cd "$type_dir"
+fi
+
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ../..
+
 make -r -s -j8
